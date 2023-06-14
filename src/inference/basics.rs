091fca58fdd0ddc6cab8e51052b4aeb3b6c987b9
edenfrenkel <eden060398@gmail.com>
2023-06-14 18:55:58 +0300

    Add simulations and gradual transition queries
    Add functionality to run simulations of a given transition system, from a specified state, according to certain branching parameters. In addition, enable gradual transition queries, which begin with an empty pre-state ('true') and gradually add hypotheses that do not satify SAT results, thereby possibly proving SAT or UNSAT with simpler queries.
    
    Signed-off-by: edenfrenkel <eden060398@gmail.com>

--- src/inference/basics.rs
+++ src/inference/basics.rs
@@ -2,7 +2,10 @@
 // SPDX-License-Identifier: BSD-2-Clause
 
 use itertools::Itertools;
-use std::collections::{HashMap, HashSet};
+use std::{
+    collections::{HashMap, HashSet},
+    sync::{Arc, RwLock},
+};
 
 use crate::{
     fly::{
@@ -15,6 +18,8 @@ use crate::{
     verify::SolverConf,
 };
 
+use rayon::prelude::*;
+
 /// A first-order module is represented using first-order formulas,
 /// namely single-vocabulary axioms, initial assertions and safety assertions,
 /// and double-vocabulary transition assertions.
@@ -26,15 +31,17 @@ pub struct FOModule {
     pub transitions: Vec<Term>,
     pub safeties: Vec<Term>,
     disj: bool,
+    gradual: bool,
 }
 
 pub enum TransCexResult {
     CTI(Model, Model),
     UnsatCore(HashSet<usize>),
+    Cancelled,
 }
 
 impl FOModule {
-    pub fn new(m: &Module, disj: bool) -> Self {
+    pub fn new(m: &Module, disj: bool, gradual: bool) -> Self {
         let mut fo = FOModule {
             signature: m.signature.clone(),
             axioms: vec![],
@@ -42,6 +49,7 @@ impl FOModule {
             transitions: vec![],
             safeties: vec![],
             disj,
+            gradual,
         };
 
         for statement in &m.statements {
@@ -72,6 +80,21 @@ impl FOModule {
         fo
     }
 
+    fn disj_trans(&self) -> Vec<Vec<&Term>> {
+        if self.disj {
+            self.transitions
+                .iter()
+                .map(|t| match t {
+                    Term::NAryOp(NOp::Or, args) => args.iter().collect_vec(),
+                    _ => vec![t],
+                })
+                .multi_cartesian_product()
+                .collect_vec()
+        } else {
+            vec![self.transitions.iter().collect_vec()]
+        }
+    }
+
     pub fn init_cex(&self, conf: &SolverConf, t: &Term) -> Option<Model> {
         self.implication_cex(conf, &self.inits, t)
     }
@@ -83,90 +106,110 @@ impl FOModule {
         t: &Term,
         with_init: bool,
         with_safety: bool,
+        cancel: Option<Arc<RwLock<bool>>>,
     ) -> TransCexResult {
-        let disj_trans = if self.disj {
-            self.transitions
-                .iter()
-                .map(|t| match t {
-                    Term::NAryOp(NOp::Or, args) => args.iter().collect_vec(),
-                    _ => vec![t],
-                })
-                .multi_cartesian_product()
-                .collect_vec()
+        let cancelled = || match &cancel {
+            None => false,
+            Some(lock) => *lock.read().unwrap(),
+        };
+        let mut participants: Vec<usize> = if self.gradual {
+            vec![]
         } else {
-            vec![self.transitions.iter().collect_vec()]
+            (0..hyp.len()).collect()
         };
 
-        let mut core: HashSet<usize> = HashSet::new();
+        loop {
+            let mut core: HashSet<usize> = HashSet::new();
+            let mut new_participants: HashSet<usize> = HashSet::new();
+            for trans in self.disj_trans() {
+                if cancelled() {
+                    return TransCexResult::Cancelled;
+                }
 
-        for trans in disj_trans {
-            let mut solver = conf.solver(&self.signature, 2);
-            let mut assumptions = HashMap::new();
+                let mut solver = conf.solver(&self.signature, 2);
+                let mut assumptions = HashMap::new();
 
-            let mut prestate = vec![];
-            for (i, a) in hyp.iter().enumerate() {
-                let ind = solver.get_indicator(i.to_string().as_str());
-                assumptions.insert(ind.clone(), true);
-                prestate.push(Term::BinOp(BinOp::Iff, Box::new(ind), Box::new(a.clone())));
-            }
-            if with_init {
-                let init = Term::and(self.inits.iter().cloned());
-                solver.assert(&Term::or([init, Term::and(prestate)]));
-            } else {
-                solver.assert(&Term::and(prestate));
-            }
+                let mut prestate = vec![];
+                for &i in &participants {
+                    let ind = solver.get_indicator(i.to_string().as_str());
+                    assumptions.insert(ind.clone(), true);
+                    prestate.push(Term::BinOp(
+                        BinOp::Iff,
+                        Box::new(ind),
+                        Box::new(hyp[i].clone()),
+                    ));
+                }
 
-            for a in self.axioms.iter() {
-                solver.assert(a);
-                solver.assert(&Next::new(&self.signature).prime(a));
-            }
+                if with_init {
+                    let init = Term::and(self.inits.iter().cloned());
+                    solver.assert(&Term::or([init, Term::and(prestate)]));
+                } else {
+                    solver.assert(&Term::and(prestate));
+                }
 
-            for a in trans {
-                solver.assert(a);
-            }
+                for a in self.axioms.iter() {
+                    solver.assert(a);
+                    solver.assert(&Next::new(&self.signature).prime(a));
+                }
 
-            if with_safety {
-                for a in self.safeties.iter() {
+                for a in trans {
                     solver.assert(a);
                 }
-            }
 
-            if with_init {
-                let init = Term::and(self.inits.iter().cloned());
-                solver.assert(&Term::negate(Next::new(&self.signature).prime(&init)));
-            }
-            solver.assert(&Term::negate(Next::new(&self.signature).prime(t)));
-
-            let resp = solver.check_sat(assumptions).expect("error in solver");
-            match resp {
-                SatResp::Sat => {
-                    let mut states = solver.get_minimal_model().into_iter();
-                    let pre = states.next().unwrap();
-                    let post = states.next().unwrap();
-                    assert_eq!(states.next(), None);
-
-                    return TransCexResult::CTI(pre, post);
-                }
-                SatResp::Unsat => {
-                    for ind in solver.get_unsat_core() {
-                        match ind.0 {
-                            Term::Id(s) => {
-                                core.insert(s[6..].parse().unwrap());
-                            }
-                            _ => (),
-                        }
+                if with_safety {
+                    for a in self.safeties.iter() {
+                        solver.assert(a);
                     }
                 }
-                SatResp::Unknown(reason) => panic!("sat solver returned unknown: {reason}"),
+
+                if with_init {
+                    let init = Term::and(self.inits.iter().cloned());
+                    solver.assert(&Term::negate(Next::new(&self.signature).prime(&init)));
+                }
+
+                solver.assert(&Term::negate(Next::new(&self.signature).prime(t)));
+
+                let resp = solver.check_sat(assumptions).expect("error in solver");
+                match resp {
+                    SatResp::Sat => {
+                        let mut states = solver.get_minimal_model().into_iter();
+                        let pre = states.next().unwrap();
+                        let post = states.next().unwrap();
+                        assert_eq!(states.next(), None);
+
+                        if let Some(i) = (0..hyp.len())
+                            .find(|i| !participants.contains(i) && pre.eval(&hyp[*i]) == 0)
+                        {
+                            new_participants.insert(i);
+                        } else {
+                            return TransCexResult::CTI(pre, post);
+                        }
+                    }
+                    SatResp::Unsat => {
+                        for ind in solver.get_unsat_core() {
+                            match ind.0 {
+                                Term::Id(s) => {
+                                    core.insert(s[6..].parse().unwrap());
+                                }
+                                _ => (),
+                            }
+                        }
+                    }
+                    SatResp::Unknown(reason) => panic!("sat solver returned unknown: {reason}"),
+                }
+            }
+
+            if new_participants.is_empty() {
+                return TransCexResult::UnsatCore(core);
+            } else {
+                participants.extend(new_participants);
             }
         }
-
-        TransCexResult::UnsatCore(core)
     }
 
     pub fn trans_safe_cex(&self, conf: &SolverConf, hyp: &[Term]) -> Option<Model> {
         for s in self.safeties.iter() {
-            if let TransCexResult::CTI(model, _) = self.trans_cex(conf, hyp, s, false, true) {
+            if let TransCexResult::CTI(model, _) = self.trans_cex(conf, hyp, s, false, true, None) {
                 return Some(model);
             }
         }
@@ -193,6 +236,75 @@ impl FOModule {
             SatResp::Unknown(_) => panic!(),
         }
     }
+
+    pub fn simulate_from(
+        &self,
+        conf: &SolverConf,
+        state: &Model,
+        width: usize,
+        depth: usize,
+    ) -> Vec<Model> {
+        let mut samples = vec![];
+        assert!(depth >= 1);
+
+        let disj_trans = self.disj_trans();
+        let state_term = state.to_term();
+
+        let mut solver = conf.solver(&self.signature, 2);
+        solver.assert(&state_term);
+        for a in &self.axioms {
+            solver.assert(&Next::new(&self.signature).prime(a));
+        }
+
+        let mut unblocked_trans: HashSet<usize> = HashSet::from_iter(0..disj_trans.len());
+        while !unblocked_trans.is_empty() && samples.len() < width {
+            for i in unblocked_trans.iter().copied().collect_vec() {
+                if samples.len() >= width {
+                    break;
+                }
+
+                let mut new_sample = None;
+                solver.push();
+                for t in &disj_trans[i] {
+                    solver.assert(t);
+                }
+
+                let resp = solver.check_sat(HashMap::new()).expect("error in solver");
+
+                match resp {
+                    SatResp::Sat => {
+                        let mut states = solver.get_model();
+                        assert_eq!(states.len(), 2);
+
+                        new_sample = states.pop();
+                    }
+                    SatResp::Unsat => {
+                        unblocked_trans.remove(&i);
+                    }
+                    SatResp::Unknown(reason) => panic!("sat solver returned unknown: {reason}"),
+                }
+
+                solver.pop();
+
+                if let Some(sample) = new_sample {
+                    solver.assert(&Term::negate(
+                        Next::new(&self.signature).prime(&sample.to_term()),
+                    ));
+                    samples.push(sample);
+                }
+            }
+        }
+
+        if depth > 1 {
+            let mut deep_samples: Vec<Model> = samples
+                .par_iter()
+                .flat_map(|sample| self.simulate_from(conf, sample, width, depth - 1))
+                .collect();
+            samples.append(&mut deep_samples);
+        }
+
+        samples
+    }
 }
 
 pub enum QfBody {
@@ -217,6 +329,13 @@ pub struct InferenceConfig {
 
     pub nesting: Option<usize>,
     pub include_eq: bool,
+
+    pub disj: bool,
+    pub gradual: bool,
+    pub indiv: bool,
+
+    pub extend_width: Option<usize>,
+    pub extend_depth: Option<usize>,
 }
 
 pub fn parse_quantifier(
