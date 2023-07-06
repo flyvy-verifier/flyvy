4d65a7c4de1fd9b95109e0665346bafe3f75bb62
James R. Wilcox <wilcoxjay@gmail.com>
2023-07-06 11:51:58 -0700

    Add integration test for updr determinism
    Signed-off-by: James R. Wilcox <wilcoxjay@gmail.com>

---
+++ tests/test_query_determinism.rs
@@ -0,0 +1,41 @@
+// Copyright 2022-2023 VMware, Inc.
+// SPDX-License-Identifier: BSD-2-Clause
+
+use std::process::Command;
+
+fn temporal_verifier() -> Command {
+    let mut cmd = Command::new("./target/debug/temporal-verifier");
+    cmd.arg("--color=never");
+    cmd
+}
+
+#[test]
+fn updr_determinism() {
+    let mut expected_output: Option<String> = None;
+    let NUM_ITERS = 2; // ideally this would be higher, but it's too slow at ~6 seconds per iteration
+    for i in 0..NUM_ITERS {
+        println!("updr determinism iteration {}", i);
+        let out = temporal_verifier()
+            .arg("updr-verify")
+            .arg("examples/lockserver.fly")
+            .arg("--solver-seed=1")
+            .output()
+            .expect("could not run temporal-verifier");
+
+        assert!(out.status.success(), "temporal-verifier should succeed");
+
+        let stdout = String::from_utf8(out.stdout).expect("non-utf8 output");
+        let stderr = String::from_utf8(out.stderr).expect("non-utf8 output");
+
+        let combined_stdout_stderr = format!("{stdout}\n======== STDERR: ===========\n{stderr}");
+
+        match expected_output {
+            Some(ref expected) => {
+                assert_eq!(expected, &combined_stdout_stderr);
+            }
+            None => {
+                expected_output = Some(combined_stdout_stderr);
+            }
+        }
+    }
+}
