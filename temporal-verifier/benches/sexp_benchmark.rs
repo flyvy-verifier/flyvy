d451ce63d4c8902ecc1362adbb862472189e148d
Alex Fischman <alexdfischman@gmail.com>
2023-07-06 15:58:38 -0700

    Fix file paths in sexp_benchmarks
    Signed-off-by: Alex Fischman <alexdfischman@gmail.com>

--- temporal-verifier/benches/sexp_benchmark.rs
+++ temporal-verifier/benches/sexp_benchmark.rs
@@ -7,13 +7,13 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use solver::sexp;
 
 pub fn sexp_parse_benchmark(c: &mut Criterion) {
-    let s = fs::read_to_string("tests/issue_5_model.sexp").expect("could not open sexp file");
+    let s = fs::read_to_string("../tests/issue_5_model.sexp").expect("could not open sexp file");
     c.bench_function("sexp::parse", |b| b.iter(|| sexp::parse(&s)));
 }
 
 pub fn sexp_to_string_benchmark(c: &mut Criterion) {
     let sexp_str =
-        fs::read_to_string("tests/issue_5_model.sexp").expect("could not open sexp file");
+        fs::read_to_string("../tests/issue_5_model.sexp").expect("could not open sexp file");
     let s = sexp::parse(&sexp_str).expect("could not parse sexp");
     c.bench_function("sexp::ToString", |b| b.iter(|| s.to_string()));
 }
