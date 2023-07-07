5cce4ab85e9135c7916f394f8cf55a962f1636a6
Alex Fischman <alexdfischman@gmail.com>
2023-07-07 12:18:21 -0700

    Disable missing doc warnings for inference and temporal-verifier crates
    Signed-off-by: Alex Fischman <alexdfischman@gmail.com>

--- inference/src/lib.rs
+++ inference/src/lib.rs
@@ -12,7 +12,7 @@
 #![allow(clippy::upper_case_acronyms)]
 #![allow(clippy::type_complexity)]
 // documentation-related lints (only checked when running rustdoc)
-#![warn(missing_docs)]
+// #![warn(missing_docs)]
 #![allow(rustdoc::private_intra_doc_links)]
 #![deny(rustdoc::broken_intra_doc_links)]
 
