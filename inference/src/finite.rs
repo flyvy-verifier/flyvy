93a42bacc06a4e24cfca1c7afd482d8784b21460
Alex Fischman <alexdfischman@gmail.com>
2023-07-10 16:20:10 -0700

    Autoformat
    Signed-off-by: Alex Fischman <alexdfischman@gmail.com>

--- inference/src/finite.rs
+++ inference/src/finite.rs
@@ -4,21 +4,21 @@
 //! Infer an inductive invariant from the set of reachable states
 //! with small sort bounds.
 
-use std::collections::HashMap;
 use bounded::bdd::*;
 use fly::syntax::*;
+use std::collections::HashMap;
 
 pub fn invariant(module: &Module) -> Result<Option<Term>, CheckerError> {
-	let mut universe = HashMap::new();
-	for sort in &module.signature.sorts {
-		universe.insert(sort.clone(), 2);
-	}
+    let mut universe = HashMap::new();
+    for sort in &module.signature.sorts {
+        universe.insert(sort.clone(), 2);
+    }
 
     let _bdd = match check(module, &universe, None, false) {
-    	Ok(CheckerAnswer::Convergence(bdd)) => bdd,
-    	Ok(CheckerAnswer::Counterexample(_)) => return Ok(None),
-    	Ok(CheckerAnswer::Unknown) => unreachable!(),
-    	Err(e) => return Err(e),
+        Ok(CheckerAnswer::Convergence(bdd)) => bdd,
+        Ok(CheckerAnswer::Counterexample(_)) => return Ok(None),
+        Ok(CheckerAnswer::Unknown) => unreachable!(),
+        Err(e) => return Err(e),
     };
 
     todo!()
