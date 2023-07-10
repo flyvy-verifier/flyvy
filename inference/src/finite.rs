d6ace414943162ed6ce420a7af61c6fc8039c800
Alex Fischman <alexdfischman@gmail.com>
2023-07-10 16:19:08 -0700

    Add finite invariant inference module
    Co-authored-by: Alex Fischman <alexdfischman@gmail.com>
    Co-authored-by: James R. Wilcox <wilcoxjay@gmail.com>
    Signed-off-by: Alex Fischman <alexdfischman@gmail.com>
    Signed-off-by: James R. Wilcox <wilcoxjay@gmail.com>

---
+++ inference/src/finite.rs
@@ -0,0 +1,25 @@
+// Copyright 2022-2023 VMware, Inc.
+// SPDX-License-Identifier: BSD-2-Clause
+
+//! Infer an inductive invariant from the set of reachable states
+//! with small sort bounds.
+
+use std::collections::HashMap;
+use bounded::bdd::*;
+use fly::syntax::*;
+
+pub fn invariant(module: &Module) -> Result<Option<Term>, CheckerError> {
+	let mut universe = HashMap::new();
+	for sort in &module.signature.sorts {
+		universe.insert(sort.clone(), 2);
+	}
+
+    let _bdd = match check(module, &universe, None, false) {
+    	Ok(CheckerAnswer::Convergence(bdd)) => bdd,
+    	Ok(CheckerAnswer::Counterexample(_)) => return Ok(None),
+    	Ok(CheckerAnswer::Unknown) => unreachable!(),
+    	Err(e) => return Err(e),
+    };
+
+    todo!()
+}
