356dce672f5a900eba7b691d72b93e4ec150c0e5
Alex Fischman <alexdfischman@gmail.com>
2023-07-10 17:27:25 -0700

    Convert BDD to Term
    Signed-off-by: Alex Fischman <alexdfischman@gmail.com>

--- inference/src/finite.rs
+++ inference/src/finite.rs
@@ -4,6 +4,7 @@
 //! Infer an inductive invariant from the set of reachable states
 //! with small sort bounds.
 
+use biodivine_lib_bdd::boolean_expression::BooleanExpression;
 use bounded::bdd::*;
 use fly::syntax::*;
 use std::collections::HashMap;
@@ -12,14 +13,68 @@ pub fn invariant(
     module: &Module,
     universe: HashMap<String, usize>,
 ) -> Result<Option<Term>, CheckerError> {
-    let (_bdd, _context) = match check(module, &universe, None, false) {
+    let (bdd, context) = match check(module, &universe, None, false) {
         Ok(CheckerAnswer::Convergence(bdd, context)) => (bdd, context),
         Ok(CheckerAnswer::Counterexample(_)) => return Ok(None),
         Ok(CheckerAnswer::Unknown) => unreachable!(),
         Err(e) => return Err(e),
     };
 
+    let mut next_binding = 0;
+    let mut bindings: HashMap<(&str, usize), Term> = HashMap::new();
+    for (sort, bound) in &universe {
+        for i in 0..*bound {
+            bindings.insert((sort, i), Term::Id(format!("${}", next_binding)));
+            next_binding += 1;
+        }
+    }
+
+    let mut reverse: HashMap<String, Term> = HashMap::new();
+    for (relation, map) in context.indices {
+        for (elements, (i, _mutable)) in map {
+            let name = context.bdds.name_of(context.vars[i]);
+            let args = module
+                .signature
+                .relations
+                .iter()
+                .find(|r| r.name == relation)
+                .unwrap()
+                .args
+                .iter()
+                .zip(elements)
+                .map(|(sort, element)| match sort {
+                    Sort::Id(sort) => bindings[&(sort.as_str(), element)].clone(),
+                    Sort::Bool => match element {
+                        0 => Term::Literal(false),
+                        1 => Term::Literal(true),
+                        _ => unreachable!(),
+                    },
+                });
+            let term = Term::App(relation.to_string(), 0, args.collect());
+            reverse.insert(name, term);
+        }
+    }
+
+    let ast = bdd.to_boolean_expression(&context.bdds);
+    let ast = to_term(ast, &reverse);
+
+    println!("{:?}", ast);
+
     todo!()
+}
+
+fn to_term(term: BooleanExpression, vars_to_terms: &HashMap<String, Term>) -> Term {
+    let go = |term| to_term(term, vars_to_terms);
+    use BooleanExpression::*;
+    match term {
+        Const(b) => Term::Literal(b),
+        Variable(name) => vars_to_terms.get(&name).unwrap().clone(),
+        Not(term) => Term::UnaryOp(UOp::Not, Box::new(go(*term))),
+        And(a, b) => Term::NAryOp(NOp::And, vec![go(*a), go(*b)]),
+        Or(a, b) => Term::NAryOp(NOp::Or, vec![go(*a), go(*b)]),
+        // Bdd::to_boolean_expression never produces Xor, Imp, or Iff
+        Xor(..) | Imp(..) | Iff(..) => unreachable!(),
+    }
 }
 
 #[cfg(test)]
