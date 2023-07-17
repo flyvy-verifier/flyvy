ad49068e4cfab54391eef0ab5780e4268306e7e8
Alex Fischman <alexdfischman@gmail.com>
2023-07-17 13:46:53 -0700

    Implement non-boolean-relations conversion
    Signed-off-by: Alex Fischman <alexdfischman@gmail.com>

---
+++ fly/src/rets.rs
@@ -0,0 +1,116 @@
+// Copyright 2022-2023 VMware, Inc.
+// SPDX-License-Identifier: BSD-2-Clause
+
+//! Utility to convert all non-boolean-returning relations in a Module to boolean-returning ones.
+
+use crate::semantics::Model;
+use crate::syntax::*;
+
+impl Module {
+    /// Converts all non-boolean-returning relations to boolean-returning ones
+    /// by adding an extra argument and axioms.
+    /// Returns a closure that can take a Model in the new Module and back-convert it to
+    /// a Model in the old Module.
+    pub fn convert_non_bool_relations(&mut self) -> Box<dyn Fn(&Model) -> Model> {
+        let old_relations: Vec<RelationDecl> = self
+            .signature
+            .relations
+            .iter()
+            .filter(|r| r.sort != Sort::Bool)
+            .cloned()
+            .collect();
+
+        let mut axioms = vec![];
+        for relation in &mut self.signature.relations {
+            if relation.sort != Sort::Bool {
+                let binders: Vec<Binder> = relation
+                    .args
+                    .iter()
+                    .chain([&relation.sort, &relation.sort])
+                    .enumerate()
+                    .map(|(i, sort)| Binder {
+                        sort: sort.clone(),
+                        name: format!("__{}", i),
+                    })
+                    .collect();
+                let other_args = &binders[0..relation.args.len()];
+                let new_arg_twice = &binders[relation.args.len()..];
+                let all_args_new: Vec<_> = binders[0..=relation.args.len()]
+                    .iter()
+                    .map(|b| Term::Id(b.name.clone()))
+                    .collect();
+                let mut all_args_new_alt = all_args_new.clone();
+                *all_args_new_alt.last_mut().unwrap() =
+                    Term::Id(binders.last().unwrap().name.clone());
+
+                axioms.push(ThmStmt::Assume(Term::UnaryOp(
+                    UOp::Always,
+                    Box::new(Term::forall(
+                        other_args.iter().cloned(),
+                        Term::exists(
+                            [binders[relation.args.len()].clone()],
+                            Term::App(relation.name.clone(), 0, all_args_new.clone()),
+                        ),
+                    )),
+                )));
+                axioms.push(ThmStmt::Assume(Term::UnaryOp(
+                    UOp::Always,
+                    Box::new(Term::forall(
+                        other_args.iter().cloned(),
+                        Term::forall(
+                            new_arg_twice.iter().cloned(),
+                            Term::implies(
+                                Term::and([
+                                    Term::App(relation.name.clone(), 0, all_args_new),
+                                    Term::App(relation.name.clone(), 0, all_args_new_alt),
+                                ]),
+                                Term::equals(
+                                    Term::Id(new_arg_twice[0].name.clone()),
+                                    Term::Id(new_arg_twice[1].name.clone()),
+                                ),
+                            ),
+                        ),
+                    )),
+                )));
+
+                relation.args.push(relation.sort.clone());
+                relation.sort = Sort::Bool;
+            }
+        }
+        self.statements.splice(0..0, axioms);
+
+        Box::new(move |model| back_convert_model(model, old_relations.clone()))
+    }
+}
+
+fn back_convert_model(_model: &Model, _old_changed_relations: Vec<RelationDecl>) -> Model {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::parser::parse;
+
+    #[test]
+    fn non_bool_relations_conversion() {
+        let source1 = "
+sort s
+mutable f(sort, bool): sort
+        ";
+        let source2 = "
+sort s
+mutable f(sort, bool, sort): bool
+
+assume always forall __0:sort, __1: bool. exists __2:sort. f(__0, __1, __2)
+assume always forall __0:sort, __1: bool. forall __2:sort, __3:sort.
+    (f(__0, __1, __2) & f(__0, __1, __3)) -> (__2 = __3)
+        ";
+
+        let mut module1 = parse(source1).unwrap();
+        let _back_convert_model = module1.convert_non_bool_relations();
+
+        let module2 = parse(source2).unwrap();
+
+        assert_eq!(module1, module2);
+    }
+}
